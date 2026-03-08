f5a5793a95d03fbf1b6c265f23110243030d47c2
Sabra Fisher <sabra.leigh.fisher@gmail.com>
2026-03-08 16:48:09 -0700

    sf • Changed from internal to public to match Map class • Added Name property to identify each room • Added ConnectedRooms list to track adjacent rooms • Added constructor that accepts a room name • Added ToString() override for easy display

--- DungeonCrawlerG2/Map.cs
+++ DungeonCrawlerG2/Map.cs
@@ -8,6 +8,28 @@ namespace DungeonCrawlerG2
 {
     public class Map
     {
-
+        // define the map as a 3 x 3 grid of interconnected rooms
+        public Room[,] Rooms { get; set; }
+        public Map()
+        {
+            Rooms = new Room[3, 3]
+            {
+                { new Room("Entrance Hall"), new Room("Armory"), new Room("Library") },
+                { new Room("Dining Room"), new Room("Great Hall"), new Room("Kitchen") },
+                { new Room("Bedroom"), new Room("Study"), new Room("Treasure Room") }
+            };
+            // connect the rooms (for simplicity, we will just connect adjacent rooms)
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i > 0) Rooms[i, j].ConnectedRooms.Add(Rooms[i - 1, j]); // connect to room above
+                    if (i < 2) Rooms[i, j].ConnectedRooms.Add(Rooms[i + 1, j]); // connect to room below
+                    if (j > 0) Rooms[i, j].ConnectedRooms.Add(Rooms[i, j - 1]); // connect to room on the left
+                    if (j < 2) Rooms[i, j].ConnectedRooms.Add(Rooms[i, j + 1]); // connect to room on the right
+                }
+            }
+        }
+        
     }
 }
--- DungeonCrawlerG2/Room.cs
+++ DungeonCrawlerG2/Room.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace DungeonCrawlerG2
 {
-    internal class Room
+    public class Room
     {
+        public string Name { get; set; }
+        public List<Room> ConnectedRooms { get; set; }
+
+        public Room(string name)
+        {
+            Name = name;
+            ConnectedRooms = new List<Room>();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
